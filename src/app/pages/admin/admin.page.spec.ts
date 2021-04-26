fc757aef91e638226aa273e5fafc4d2d8e4659ee
vmanuelpm <vmanuelpm97@hotmail.com>
2021-04-26 21:45:28 +0200

    added fixes in basic test

--- src/app/pages/admin/admin.page.spec.ts
+++ src/app/pages/admin/admin.page.spec.ts
@@ -9,10 +9,14 @@ import { GameService } from '../../services/game.service';
 import { StorageService } from '../../services/storage.service';
 import { AdminPage } from './admin.page';
 import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('AdminPage', () => {
   let component: AdminPage;
   let fixture: ComponentFixture<AdminPage>;
+  let de: DebugElement;
+  let el: HTMLElement;
 
   beforeEach(
     waitForAsync(() => {
@@ -30,7 +34,14 @@ describe('AdminPage', () => {
           StorageService,
           { provide: AngularFireStorage, useValue: {} },
         ],
-      }).compileComponents();
+      })
+        .compileComponents()
+        .then(() => {
+          fixture = TestBed.createComponent(AdminPage);
+          component = fixture.componentInstance;
+          de = fixture.debugElement.nativeElement.querySelector('#formAdmin');
+          el = de.nativeElement;
+        });
 
       fixture = TestBed.createComponent(AdminPage);
       component = fixture.componentInstance;
@@ -40,5 +51,9 @@ describe('AdminPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should have button Add Game', () => {
+    expect(component.textButton).toEqual('Add Game');
   });
 });
